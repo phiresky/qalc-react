0c7c7d62884a993d1b1221292ed6061ae607cf63
phiresky <phireskyde+git@gmail.com>
2017-12-26 12:50:07 +0100

    test

--- src/GnuUnitsCategorizeHelper.tsx
+++ src/GnuUnitsCategorizeHelper.tsx
@@ -1,37 +1,46 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import 'whatwg-fetch';
-import * as mobx from 'mobx';
-import * as mobxReact from 'mobx-react';
-import DevTools from 'mobx-react-devtools';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "whatwg-fetch";
+import * as mobx from "mobx";
+import * as mobxReact from "mobx-react";
+import DevTools from "mobx-react-devtools";
 
 const dir = "./gnu-units-2.12/";
 enum Type {
-	Deleted, Normal, Heading
+	Deleted,
+	Normal,
+	Heading,
 }
 interface Box {
-	start: number; end: number; type: Type; headingLevel?: number;
+	start: number;
+	end: number;
+	type: Type;
+	headingLevel?: number;
 }
 
-function binarySearchIndex<T>(array: T[], accessor: (ele:T) => number, search: number, exact = true, min = 0, max = array.length - 1): number {
-	if(min === max)
-		if(exact && accessor(array[min]) !== search) throw Error("can't find element" + search);
+function binarySearchIndex<T>(
+	array: T[],
+	accessor: (ele: T) => number,
+	search: number,
+	exact = true,
+	min = 0,
+	max = array.length - 1,
+): number {
+	if (min === max)
+		if (exact && accessor(array[min]) !== search)
+			throw Error("can't find element" + search);
 		else return min;
-	const mid = ((min + max) / 2) |0;
-	if(accessor(array[mid]) < search)
+
+	const mid = ((min + max) / 2) | 0;
+	if (accessor(array[mid]) < search)
 		return binarySearchIndex(array, accessor, search, exact, mid + 1, max);
-	else
-		return binarySearchIndex(array, accessor, search, exact, min, mid);
+	else return binarySearchIndex(array, accessor, search, exact, min, mid);
 }
 export class CategorizeStore {
-	@mobx.observable
-	executed: any[][] = [];
-	@mobx.observable
-	boxes: Box[] = [];
-	@mobx.observable
-	selectionStart = -1;
-	@mobx.observable
-	selectionEnd = -1;
+	@mobx.observable executed: any[][] = [];
+	@mobx.observable boxes: Box[] = [];
+	@mobx.observable selectionStart = -1;
+	@mobx.observable selectionEnd = -1;
 	readonly lines: string[];
 	constructor(data: string, changes: any[][] = []) {
 		this.lines = data.split("\n");
@@ -41,7 +50,7 @@ export class CategorizeStore {
 	}
 	@mobx.action
 	executeAll(changes: any[][]) {
-		for(const change of changes) this.execute(change);
+		for (const change of changes) this.execute(change);
 	}
 	@mobx.action
 	execute(cmd: any[]) {
@@ -53,23 +62,27 @@ export class CategorizeStore {
 	@mobx.action
 	markSelection(a: number, b: number, type: Type) {
 		const [startI, endI] = this.findBounds(this.boxes, a, b);
-		for(let i = startI; i <= endI; i++) this.boxes[i].type = type;
+		for (let i = startI; i <= endI; i++) this.boxes[i].type = type;
 	}
 	@mobx.action
 	unifySelection(a: number, b: number) {
 		const [startI, endI] = this.findBounds(this.boxes, a, b);
 		const type = this.boxes[startI].type;
-		this.boxes.splice(startI, endI - startI + 1, {start: a, end: b, type});
+		this.boxes.splice(startI, endI - startI + 1, {
+			start: a,
+			end: b,
+			type,
+		});
 		this.selectionStart = -1;
-		this.selectionEnd = -1;	
+		this.selectionEnd = -1;
 	}
 	@mobx.action
 	splitSelection(a: number, b: number) {
-		const [startI, endI] = this.findBounds(this.boxes, a,b);
+		const [startI, endI] = this.findBounds(this.boxes, a, b);
 		const newBs = [] as Box[];
 		const type = this.boxes[startI].type;
-		for(let i = a; i <= b; i++) {
-			newBs.push({start:i, end: i, type});
+		for (let i = a; i <= b; i++) {
+			newBs.push({ start: i, end: i, type });
 		}
 		this.boxes.splice(startI, endI - startI + 1, ...newBs);
 		this.selectionStart = -1;
@@ -78,74 +91,109 @@ export class CategorizeStore {
 	@mobx.action
 	setHeadingDepth(a: number, b: number, headingLevel: number) {
 		const [startI, endI] = this.findBounds(this.boxes, a, b);
-		for(let i = startI; i <= endI; i++)
-			if(this.boxes[i].type === Type.Heading)
-			this.boxes[i].headingLevel = headingLevel;
+		for (let i = startI; i <= endI; i++)
+			if (this.boxes[i].type === Type.Heading)
+				this.boxes[i].headingLevel = headingLevel;
 	}
 	findBounds(boxes: Box[], l: number, r: number): [number, number] {
-		return [binarySearchIndex(boxes, x => x.start, l), binarySearchIndex(boxes, x => x.end, r)];
+		return [
+			binarySearchIndex(boxes, x => x.start, l),
+			binarySearchIndex(boxes, x => x.end, r),
+		];
 	}
 	categoryTreeOf(line: number) {
-		const boxI = Math.min(binarySearchIndex(this.boxes, x => x.start, line, false),
-			binarySearchIndex(this.boxes, x => x.end, line, false));
-		const catTree = [''] as string[];
-		for(let i = 0; i <= boxI; i++) {
+		const boxI = Math.min(
+			binarySearchIndex(this.boxes, x => x.start, line, false),
+			binarySearchIndex(this.boxes, x => x.end, line, false),
+		);
+		const catTree = [""] as string[];
+		for (let i = 0; i <= boxI; i++) {
 			const box = this.boxes[i];
 			const level = box.headingLevel || catTree.length - 1;
-			if(box.type == Type.Heading && level <= catTree.length) {
+			if (box.type == Type.Heading && level <= catTree.length) {
 				catTree.splice(level);
-				catTree.push(this.lines.slice(box.start, box.end + 1)
-						.map(line => line.replace(/\s+/g, " ").replace(/#/g, "").trim())
-					.join("\n").trim());
+				catTree.push(
+					this.lines
+						.slice(box.start, box.end + 1)
+						.map(line =>
+							line
+								.replace(/\s+/g, " ")
+								.replace(/#/g, "")
+								.trim(),
+						)
+						.join("\n")
+						.trim(),
+				);
 			}
 		}
 		catTree.shift();
 		const box = this.boxes[boxI];
-		const comment = this.lines.slice(box.start, box.end + 1).map(line => (line.split("#")[1] || "").trim()).join("\n").trim();
-		return {headings: catTree, comment};
+		const comment = this.lines
+			.slice(box.start, box.end + 1)
+			.map(line => (line.split("#")[1] || "").trim())
+			.join("\n")
+			.trim();
+		return { headings: catTree, comment };
 	}
 }
 @mobxReact.observer
-class HelperGui extends React.Component<{store: CategorizeStore}, {}> {
+class HelperGui extends React.Component<{ store: CategorizeStore }, {}> {
 	pres: Map<Node, number> = new Map();
 	store: CategorizeStore;
-	constructor(props: {store: CategorizeStore}) {
+	constructor(props: { store: CategorizeStore }) {
 		super(props);
 		this.store = this.props.store;
-		document.addEventListener("selectionchange", e => this.getSelection(document.getSelection()), false);
-		document.addEventListener("keyup", e => this.commandKey(e, String.fromCharCode(e.keyCode)));
+		document.addEventListener(
+			"selectionchange",
+			e => this.getSelection(document.getSelection()),
+			false,
+		);
+		document.addEventListener("keyup", e =>
+			this.commandKey(e, String.fromCharCode(e.keyCode)),
+		);
 		mobx.autorun("consistency check", () => this.checkConsistency());
-		mobx.reaction( () => JSON.stringify(this.store.executed), (data) => {
-			console.log("set localStorage");
-			localStorage.setItem("executed", data);
-		}, {name: "save to localStorage", fireImmediately: false, delay: 500});
+		mobx.reaction(
+			() => JSON.stringify(this.store.executed),
+			data => {
+				console.log("set localStorage");
+				localStorage.setItem("executed", data);
+			},
+			{
+				name: "save to localStorage",
+				fireImmediately: false,
+				delay: 500,
+			},
+		);
 	}
 	commandKey(e: KeyboardEvent, key: string) {
-		if(e.shiftKey || e.altKey || e.metaKey || e.ctrlKey) return;
+		if (e.shiftKey || e.altKey || e.metaKey || e.ctrlKey) return;
 		const sel = [this.store.selectionStart, this.store.selectionEnd];
-		if(sel[0] == -1 || sel[1] == -1) {
+		if (sel[0] == -1 || sel[1] == -1) {
 			console.log("no selection");
 			return;
 		}
-		if(key == 'T') {
+		if (key == "T") {
 			console.log(this.store.categoryTreeOf(sel[0]));
 			return;
-		} else if(key =='C') {
-			console.log(this.store.lines.slice(sel[0], sel[1] + 1)
-				.map(x => x.replace(/^\s*#/g, ""))
-				.join("\n"));
+		} else if (key == "C") {
+			console.log(
+				this.store.lines
+					.slice(sel[0], sel[1] + 1)
+					.map(x => x.replace(/^\s*#/g, ""))
+					.join("\n"),
+			);
 		}
-		const cmds:{[name:string]: any[]} = {
-			'J':["unifySelection", ...sel], 
-			'S':["splitSelection", ...sel],
-			'D':["markSelection", ...sel, Type.Deleted],
-			'H':["markSelection", ...sel, Type.Heading],
-			'N':["markSelection", ...sel, Type.Normal],
+		const cmds: { [name: string]: any[] } = {
+			J: ["unifySelection", ...sel],
+			S: ["splitSelection", ...sel],
+			D: ["markSelection", ...sel, Type.Deleted],
+			H: ["markSelection", ...sel, Type.Heading],
+			N: ["markSelection", ...sel, Type.Normal],
 		};
 		let cmd = cmds[key];
-		if(/[0-9]/.test(key)) cmd = ["setHeadingDepth", ...sel, +key]; 
-		else if(!cmds[key]) {
-			console.log("unknown command: "+key);
+		if (/[0-9]/.test(key)) cmd = ["setHeadingDepth", ...sel, +key];
+		else if (!cmds[key]) {
+			console.log("unknown command: " + key);
 			return;
 		}
 		this.store.execute(cmd);
@@ -154,51 +202,85 @@ class HelperGui extends React.Component<{store: CategorizeStore}, {}> {
 	@mobx.action
 	getSelection(s: Selection) {
 		const [boxes, lines] = [this.store.boxes, this.store.lines];
-		let boxStartI = this.pres.get(s.anchorNode) || this.pres.get(s.anchorNode.parentElement!);
-		let boxEndI = this.pres.get(s.focusNode) || this.pres.get(s.focusNode.parentNode!);
-		if(boxStartI === undefined || boxEndI === undefined) {
+		let boxStartI =
+			this.pres.get(s.anchorNode) ||
+			this.pres.get(s.anchorNode.parentElement!);
+		let boxEndI =
+			this.pres.get(s.focusNode) ||
+			this.pres.get(s.focusNode.parentNode!);
+		if (boxStartI === undefined || boxEndI === undefined) {
 			boxStartI = -1;
 			boxEndI = -1;
 		}
-		if(boxEndI < boxStartI) [boxStartI, boxEndI] = [boxEndI, boxStartI];
-		const [a, b] = [this.store.boxes[boxStartI].start, this.store.boxes[boxEndI].end];
+		if (boxEndI < boxStartI) [boxStartI, boxEndI] = [boxEndI, boxStartI];
+		const [a, b] = [
+			this.store.boxes[boxStartI].start,
+			this.store.boxes[boxEndI].end,
+		];
 		this.store.selectionStart = a;
 		this.store.selectionEnd = b;
 	}
 	render() {
-		const {boxes, lines} = this.store;
+		const { boxes, lines } = this.store;
 		let currentIndent = 0;
 		return (
 			<div className="container">
 				<div className="page-header">
-					<p>Select boxes with the mouse. <br />
-					Then press J to join boxes, S to split them, <br />
-					H to mark them as a heading <br/>
-					N to mark them as normal <br/>
-					D to mark them as deleted <br/>
-					T to print the category tree of the first box to the console. <br/>
-					Use 1-9 to set the level of the heading, 0 to set it to the level of the previous heading <br/>
+					<p>
+						Select boxes with the mouse. <br />
+						Then press J to join boxes, S to split them, <br />
+						H to mark them as a heading <br />
+						N to mark them as normal <br />
+						D to mark them as deleted <br />
+						T to print the category tree of the first box to the
+						console. <br />
+						Use 1-9 to set the level of the heading, 0 to set it to
+						the level of the previous heading <br />
 					</p>
-					<p>The data is saved to localStorage.executed as a list of the executed commands</p>
+					<p>
+						The data is saved to localStorage.executed as a list of
+						the executed commands
+					</p>
 				</div>
-				{boxes.map((box,i) => {
-					if(box.headingLevel) currentIndent = box.headingLevel;
-					const thisIndent = currentIndent + (box.type == Type.Heading ? 0 : 1);
-					return <pre style={Object.assign({}, typeStyles[box.type], {marginLeft:((thisIndent-1)*70)+"px"})} className={box.start >= this.store.selectionStart && box.end <= this.store.selectionEnd ? "alert alert-info": ""}
-							key={box.start+","+box.end} ref={e => this.pres.set(e!, i)}>
-						{lines.slice(box.start, box.end + 1).join("\n")}
-					</pre>
-				}
-				)}
+				{boxes.map((box, i) => {
+					if (box.headingLevel) currentIndent = box.headingLevel;
+					const thisIndent =
+						currentIndent + (box.type == Type.Heading ? 0 : 1);
+					return (
+						<pre
+							style={Object.assign({}, typeStyles[box.type], {
+								marginLeft: (thisIndent - 1) * 70 + "px",
+							})}
+							className={
+								box.start >= this.store.selectionStart &&
+								box.end <= this.store.selectionEnd
+									? "alert alert-info"
+									: ""
+							}
+							key={box.start + "," + box.end}
+							ref={e => this.pres.set(e!, i)}
+						>
+							{lines.slice(box.start, box.end + 1).join("\n")}
+						</pre>
+					);
+				})}
 			</div>
-		)
+		);
 	}
 
 	checkConsistency() {
 		let i = 0;
-		for(const box of this.store.boxes) {
-			if(box.start !== i) console.error("inconsistency: end=", i, "!=", box.start,"=start");
-			if(box.end < box.start) console.error("inconsistency box < 0", box);
+		for (const box of this.store.boxes) {
+			if (box.start !== i)
+				console.error(
+					"inconsistency: end=",
+					i,
+					"!=",
+					box.start,
+					"=start",
+				);
+			if (box.end < box.start)
+				console.error("inconsistency box < 0", box);
 			i = box.end + 1;
 		}
 	}
@@ -206,30 +288,28 @@ class HelperGui extends React.Component<{store: CategorizeStore}, {}> {
 
 const styles = {
 	box: {
-		borderBottom:"1px solid black",
-		borderRadius:"0px",
-		padding:"2px",
-		margin:0,
-		maxHeight: "300px"
+		borderBottom: "1px solid black",
+		borderRadius: "0px",
+		padding: "2px",
+		margin: 0,
+		maxHeight: "300px",
 	},
 };
 const typeStyles = {
 	[Type.Deleted]: Object.assign({}, styles.box, {
-		color: "lightgray"
+		color: "lightgray",
 	}),
-	[Type.Normal]: Object.assign({}, styles.box, {
-		
-	}),
+	[Type.Normal]: Object.assign({}, styles.box, {}),
 	[Type.Heading]: Object.assign({}, styles.box, {
 		fontWeight: "bold",
-		fontSize: "120%"
-	})
+		fontSize: "120%",
+	}),
 };
 function splitOnce(x: RegExp, str: string, trim = false): [string, string] {
 	const i = str.search(x);
-	if(i < 0) return [str, ""];
-	const [a,b] = [str.substr(0, i), str.substr(i)];
-	if(trim) return [a.trim(), b.trim()];
+	if (i < 0) return [str, ""];
+	const [a, b] = [str.substr(0, i), str.substr(i)];
+	if (trim) return [a.trim(), b.trim()];
 	else return [a, b];
 }
 function autoparseText(lines: string[]) {
@@ -240,35 +320,52 @@ function autoparseText(lines: string[]) {
 	for (let i = 0; i < lines.length; i++) {
 		let [line, comment] = splitOnce(/#/, lines[i], true);
 		let [variable, value] = splitOnce(/\s/, line, true);
-		
-		if(lastHadBackslash) {
+
+		if (lastHadBackslash) {
 			lastHadBackslash = line.endsWith("\\");
 			continue;
 		}
 		lastHadBackslash = line.endsWith("\\");
 		if (line.length > 0) {
-			if(!line.startsWith("!")) {
+			if (!line.startsWith("!")) {
 				//console.log(currentAliases, value);
-				if(currentAliases.indexOf(value) >= 0) {
+				if (currentAliases.indexOf(value) >= 0) {
 					currentAliases.push(variable);
 					continue;
 				}
 			}
-			
-		} else if(comment.length > 0) continue;
+		} else if (comment.length > 0) continue;
 		// end box
-		if(i > 0) boxes.push({start: boxStart, end: i - 1, type: currentAliases.length > 0 ? Type.Normal : i - 1 - boxStart == 0 ? Type.Deleted : Type.Heading });
+		if (i > 0)
+			boxes.push({
+				start: boxStart,
+				end: i - 1,
+				type:
+					currentAliases.length > 0
+						? Type.Normal
+						: i - 1 - boxStart == 0 ? Type.Deleted : Type.Heading,
+			});
 		boxStart = i;
 		currentAliases = [variable, value].filter(x => x.length > 0);
 	}
-	if(boxStart < lines.length) boxes.push({start: boxStart, end: lines.length - 1, type: Type.Deleted});
+	if (boxStart < lines.length)
+		boxes.push({
+			start: boxStart,
+			end: lines.length - 1,
+			type: Type.Deleted,
+		});
 	return boxes;
 }
 export function init(str: string) {
 	const actions = JSON.parse(localStorage.getItem("executed") || "[]");
 	ReactDOM.render(
 		<div>
-			<HelperGui store={new CategorizeStore(str, actions)} ref={guiInst => Object.assign(window, {guiInst})} />
+			<HelperGui
+				store={new CategorizeStore(str, actions)}
+				ref={guiInst => Object.assign(window, { guiInst })}
+			/>
 			<DevTools />
-		</div>, document.getElementById("root"));
+		</div>,
+		document.getElementById("root"),
+	);
 }
--- src/unitNumber.ts
+++ src/unitNumber.ts
@@ -1,24 +1,36 @@
-import Decimal from 'decimal.js';
-import {Tree} from './parser';
-import {TaggedString} from './output';
+import Decimal from "decimal.js";
+import { Tree } from "./parser";
+import { TaggedString } from "./output";
 export type EvaluatedNode = Tree.Node & { value: UnitNumber };
 
 // maps from dimension id to exponent
 // e.g.  m/s^2 has dimension map {m => 1, s => -2}
 class DimensionMap extends Map<DimensionId, number> {
-	private static unicodePow = '⁰¹²³⁴⁵⁶⁷⁸⁹';
+	private static unicodePow = "⁰¹²³⁴⁵⁶⁷⁸⁹";
 	static toUnicodePow(x: number) {
 		if (x === 1) return "";
-		return x.toString().split("").map(x => DimensionMap.unicodePow[+x]).join("");
+		return x
+			.toString()
+			.split("")
+			.map(x => DimensionMap.unicodePow[+x])
+			.join("");
 	}
 	static listToUnicodePow(entries: [DimensionId, number][]): TaggedString {
-		return TaggedString.join(entries.map(([id, exp]) => TaggedString.t`${Dimension.get(id)!.baseUnit}${DimensionMap.toUnicodePow(exp)}`), " ");
+		return TaggedString.join(
+			entries.map(
+				([id, exp]) =>
+					TaggedString.t`${
+						Dimension.get(id)!.baseUnit
+					}${DimensionMap.toUnicodePow(exp)}`,
+			),
+			" ",
+		);
 	}
 	toString(): string {
 		return this.toTaggedString() + "";
 	}
 	toTaggedString(): TaggedString {
-		const {pos, neg} = this.splitPosNeg();
+		const { pos, neg } = this.splitPosNeg();
 		let str = DimensionMap.listToUnicodePow(pos);
 		if (neg.length > 0) {
 			str.vals.push(" / ");
@@ -28,12 +40,13 @@ class DimensionMap extends Map<DimensionId, number> {
 	}
 	splitPosNeg() {
 		const pos = [...this].filter(([id, exp]) => exp > 0);
-		const neg = [...this].filter(([id, exp]) => exp < 0); neg.forEach(x => x[1] *= -1);
+		const neg = [...this].filter(([id, exp]) => exp < 0);
+		neg.forEach(x => (x[1] *= -1));
 		return { pos, neg };
 	}
-	static join(...list: { dimensions: DimensionMap, factor: number }[]) {
+	static join(...list: { dimensions: DimensionMap; factor: number }[]) {
 		const map: DimensionMap = new DimensionMap();
-		for (const {dimensions, factor} of list) {
+		for (const { dimensions, factor } of list) {
 			for (const [dimension, exponent] of dimensions) {
 				const newFactor = (map.get(dimension) || 0) + exponent * factor;
 				if (newFactor === 0) map.delete(dimension);
@@ -46,17 +59,24 @@ class DimensionMap extends Map<DimensionId, number> {
 		if (this.size > 0) throw Error(str + " must be dimensionless");
 	}
 	equals(d: DimensionMap) {
-		const diff = DimensionMap.join({dimensions:this, factor:1}, {dimensions:d, factor:-1});
-		return diff.size === 0; 
+		const diff = DimensionMap.join(
+			{ dimensions: this, factor: 1 },
+			{ dimensions: d, factor: -1 },
+		);
+		return diff.size === 0;
 	}
 	assertEqual(d: DimensionMap) {
-		if(!this.equals(d)) throw Error("dimensions must be the same");
+		if (!this.equals(d)) throw Error("dimensions must be the same");
 	}
 	toMismatchString() {
-		const {pos: tooMuch, neg: notEnough} = this.splitPosNeg();
-		if (tooMuch.length === 0) return "missing " + new DimensionMap(notEnough);
-		if (notEnough.length === 0) return "don't want " + new DimensionMap(tooMuch);
-		return `have ${new DimensionMap(tooMuch)}, want ${new DimensionMap(notEnough)}`;
+		const { pos: tooMuch, neg: notEnough } = this.splitPosNeg();
+		if (tooMuch.length === 0)
+			return "missing " + new DimensionMap(notEnough);
+		if (notEnough.length === 0)
+			return "don't want " + new DimensionMap(tooMuch);
+		return `have ${new DimensionMap(tooMuch)}, want ${new DimensionMap(
+			notEnough,
+		)}`;
 	}
 }
 
@@ -64,32 +84,52 @@ export class UnitNumber {
 	readonly value: Decimal;
 	readonly dimensions: DimensionMap;
 	readonly id: string | null;
-	constructor(value: Decimal | number | string | null, dimensions: DimensionMap | null = new DimensionMap(), id?: string | null) {
+	constructor(
+		value: Decimal | number | string | null,
+		dimensions: DimensionMap | null = new DimensionMap(),
+		id?: string | null,
+	) {
 		if (value !== null) this.value = new Decimal(value);
 		if (dimensions !== null) this.dimensions = dimensions;
 		this.id = id === undefined ? null : id;
 	}
 	mul(other: UnitNumber): UnitNumber {
 		if (other.isSpecial()) return other.mul(this, true);
-		return new UnitNumber(this.value.times(other.value), DimensionMap.join(
-			{ dimensions: this.dimensions, factor: 1 },
-			{ dimensions: other.dimensions, factor: 1 }
-		), null);
+		return new UnitNumber(
+			this.value.times(other.value),
+			DimensionMap.join(
+				{ dimensions: this.dimensions, factor: 1 },
+				{ dimensions: other.dimensions, factor: 1 },
+			),
+			null,
+		);
 	}
 	div(other: UnitNumber): UnitNumber {
 		if (other.isSpecial()) return other.div(this, true);
 		let name: string | null = null;
-		if (this.dimensions.size == 0 && other.dimensions.size == 0) name = this.value.toString() + "|" + other.value.toString();
-		return new UnitNumber(this.value.div(other.value), DimensionMap.join(
-			{ dimensions: this.dimensions, factor: 1 },
-			{ dimensions: other.dimensions, factor: -1 }
-		), name);
+		if (this.dimensions.size == 0 && other.dimensions.size == 0)
+			name = this.value.toString() + "|" + other.value.toString();
+		return new UnitNumber(
+			this.value.div(other.value),
+			DimensionMap.join(
+				{ dimensions: this.dimensions, factor: 1 },
+				{ dimensions: other.dimensions, factor: -1 },
+			),
+			name,
+		);
 	}
 	plus(other: UnitNumber, factor = 1): UnitNumber {
 		if (other.isSpecial()) return other.plus(this, factor, true);
 		const dimensionDifference = this.div(other).dimensions;
-		if (dimensionDifference.size > 0) throw Error("Dimensions don't match: " + dimensionDifference.toMismatchString());
-		return new UnitNumber(this.value.plus(other.value.times(factor)), this.dimensions);
+		if (dimensionDifference.size > 0)
+			throw Error(
+				"Dimensions don't match: " +
+					dimensionDifference.toMismatchString(),
+			);
+		return new UnitNumber(
+			this.value.plus(other.value.times(factor)),
+			this.dimensions,
+		);
 	}
 	minus(other: UnitNumber) {
 		return this.plus(other, -1);
@@ -110,21 +150,35 @@ export class UnitNumber {
 	toTaggedString() {
 		if (this.id) return new TaggedString(this);
 		else {
-			if (this.value.equals(1) && this.dimensions.size == 0) return new TaggedString("1");
+			if (this.value.equals(1) && this.dimensions.size == 0)
+				return new TaggedString("1");
 			const v = this.value.equals(1) ? "" : this.value.toPrecision(10);
-			return TaggedString.t`${v}${v && this.dimensions.size > 0 ? " " : ""}${this.dimensions.toTaggedString()}`;
+			return TaggedString.t`${v}${
+				v && this.dimensions.size > 0 ? " " : ""
+			}${this.dimensions.toTaggedString()}`;
 		}
 	}
 	pow(factor: number | Decimal | UnitNumber): UnitNumber {
-		if (typeof factor === 'number' || factor instanceof Decimal)
-			return new UnitNumber(this.value.pow(factor), DimensionMap.join({ dimensions: this.dimensions, factor: typeof factor === 'number' ? factor : factor.toNumber() }));
+		if (typeof factor === "number" || factor instanceof Decimal)
+			return new UnitNumber(
+				this.value.pow(factor),
+				DimensionMap.join({
+					dimensions: this.dimensions,
+					factor:
+						typeof factor === "number" ? factor : factor.toNumber(),
+				}),
+			);
 		else if (factor.isSpecial()) return factor.pow(this, true);
-		else if (factor.dimensions.size > 0) throw Error("power must be dimensionless");
+		else if (factor.dimensions.size > 0)
+			throw Error("power must be dimensionless");
 		else return this.pow(factor.value);
 	}
 	convertTo(unit: UnitNumber): UnitNumber {
 		const d = this.div(unit);
-		if (d.dimensions.size > 0) throw Error("Dimensions don't match: " + d.dimensions.toMismatchString());
+		if (d.dimensions.size > 0)
+			throw Error(
+				"Dimensions don't match: " + d.dimensions.toMismatchString(),
+			);
 		return d.mul(unit);
 	}
 	static createBaseUnit(dimensionName: string) {
@@ -133,19 +187,39 @@ export class UnitNumber {
 }
 
 export class SpecialUnitNumber extends UnitNumber {
-	get value(): Decimal { throw Error("can't get function.value") }
-	get dimensions(): DimensionMap { throw Error("can't get function.dimensions") }
+	get value(): Decimal {
+		throw Error("can't get function.value");
+	}
+	get dimensions(): DimensionMap {
+		throw Error("can't get function.dimensions");
+	}
 	fn: ((arg: UnitNumber) => UnitNumber) | null;
 	fnTree: Tree.Node | null;
 	readonly inverse: SpecialUnitNumber;
-	constructor(fnTree: Tree.Node | null, fn: ((arg: UnitNumber) => UnitNumber) | null, inverse: SpecialUnitNumber | null, id: string | null, inverseFnTree: Tree.Node | null = null, inverseFn: ((arg: UnitNumber) => UnitNumber) | null = null) {
+	constructor(
+		fnTree: Tree.Node | null,
+		fn: ((arg: UnitNumber) => UnitNumber) | null,
+		inverse: SpecialUnitNumber | null,
+		id: string | null,
+		inverseFnTree: Tree.Node | null = null,
+		inverseFn: ((arg: UnitNumber) => UnitNumber) | null = null,
+	) {
 		super(null, null, id);
 		this.fnTree = fnTree;
 		this.fn = fn;
-		this.inverse = inverse || new SpecialUnitNumber(inverseFnTree, inverseFn, this, id + "^-1");
+		this.inverse =
+			inverse ||
+			new SpecialUnitNumber(inverseFnTree, inverseFn, this, id + "^-1");
 	}
 	withIdentifier(id: string, alsoInverseId = true): SpecialUnitNumber {
-		return new SpecialUnitNumber(this.fnTree, this.fn, null, id, this.inverse.fnTree, this.inverse.fn);
+		return new SpecialUnitNumber(
+			this.fnTree,
+			this.fn,
+			null,
+			id,
+			this.inverse.fnTree,
+			this.inverse.fn,
+		);
 	}
 	mul(other: UnitNumber, reversed = false): UnitNumber {
 		if (this.fn) return this.fn(other);
@@ -153,7 +227,7 @@ export class SpecialUnitNumber extends UnitNumber {
 	}
 	div(other: UnitNumber, reversed = false): UnitNumber {
 		if (reversed) return this.inverse.mul(other);
-		else throw Error(`can't divide function ${this} with ${other}`)
+		else throw Error(`can't divide function ${this} with ${other}`);
 	}
 	plus(other: UnitNumber, factor = 1, reversed = false): UnitNumber {
 		throw Error(`can't add ${this} and ${other}`);
@@ -164,8 +238,10 @@ export class SpecialUnitNumber extends UnitNumber {
 		throw Error(`can't pow ${this} with ${other}`);
 	}
 	assign(other: UnitNumber) {
-		if (other.isSpecial()) {this.fn = other.fn; this.fnTree = other.fnTree; }
-		else throw Error("can't assign non-function to function");
+		if (other.isSpecial()) {
+			this.fn = other.fn;
+			this.fnTree = other.fnTree;
+		} else throw Error("can't assign non-function to function");
 	}
 	isSpecial(): this is SpecialUnitNumber {
 		return true;
@@ -181,14 +257,20 @@ export class SpecialUnitNumber extends UnitNumber {
 }
 
 // <hack href="https://github.com/Microsoft/TypeScript/issues/202">
-module AsDimensionId { export const enum Brand { } }
-interface AsDimensionId { '': AsDimensionId.Brand; }
+namespace AsDimensionId {
+	export const enum Brand {}
+}
+interface AsDimensionId {
+	"": AsDimensionId.Brand;
+}
 type DimensionId = number & AsDimensionId;
 // </hack>
 
 class Dimension {
 	private static dimensions = new Map<DimensionId, Dimension>();
-	static get(id: DimensionId) { return Dimension.dimensions.get(id); }
+	static get(id: DimensionId) {
+		return Dimension.dimensions.get(id);
+	}
 	readonly id: DimensionId;
 	readonly name: string;
 	readonly baseUnit: UnitNumber;
@@ -196,6 +278,10 @@ class Dimension {
 		this.id = Dimension.dimensions.size as DimensionId;
 		this.name = name;
 		Dimension.dimensions.set(this.id, this);
-		this.baseUnit = new UnitNumber(1, new DimensionMap([[this.id, 1]]), this.name);
+		this.baseUnit = new UnitNumber(
+			1,
+			new DimensionMap([[this.id, 1]]),
+			this.name,
+		);
 	}
 }
